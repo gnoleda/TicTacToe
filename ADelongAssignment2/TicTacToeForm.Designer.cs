61af8174f4b5b4f24c6c066d3e339618354224ae
Aubrey Delong <Adelong7545@conestogac.on.ca>
2017-11-20 16:53:32 -0500

    TicTacToe game created in Object-Oriented Gaming Class

---
+++ ADelongAssignment2/TicTacToeForm.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ADelongAssignment2
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic0 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.txtPlayer1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlayer2 = new System.Windows.Forms.TextBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ADelongAssignment2.Properties.Resources.board_2_;
+            this.pictureBox1.Location = new System.Drawing.Point(78, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(395, 399);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackColor = System.Drawing.Color.Black;
+            this.pic2.Location = new System.Drawing.Point(354, 76);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(97, 102);
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic6
+            // 
+            this.pic6.BackColor = System.Drawing.Color.Black;
+            this.pic6.Location = new System.Drawing.Point(95, 334);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(97, 102);
+            this.pic6.TabIndex = 2;
+            this.pic6.TabStop = false;
+            this.pic6.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.Color.Black;
+            this.pic1.Location = new System.Drawing.Point(226, 76);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(97, 102);
+            this.pic1.TabIndex = 4;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.BackColor = System.Drawing.Color.Black;
+            this.pic4.Location = new System.Drawing.Point(226, 204);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(97, 102);
+            this.pic4.TabIndex = 5;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic8
+            // 
+            this.pic8.BackColor = System.Drawing.Color.Black;
+            this.pic8.Location = new System.Drawing.Point(354, 334);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(97, 102);
+            this.pic8.TabIndex = 6;
+            this.pic8.TabStop = false;
+            this.pic8.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic0
+            // 
+            this.pic0.BackColor = System.Drawing.Color.Black;
+            this.pic0.Location = new System.Drawing.Point(95, 76);
+            this.pic0.Name = "pic0";
+            this.pic0.Size = new System.Drawing.Size(97, 102);
+            this.pic0.TabIndex = 7;
+            this.pic0.TabStop = false;
+            this.pic0.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic3
+            // 
+            this.pic3.BackColor = System.Drawing.Color.Black;
+            this.pic3.Location = new System.Drawing.Point(95, 204);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(97, 102);
+            this.pic3.TabIndex = 8;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic7
+            // 
+            this.pic7.BackColor = System.Drawing.Color.Black;
+            this.pic7.Location = new System.Drawing.Point(226, 334);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(97, 102);
+            this.pic7.TabIndex = 9;
+            this.pic7.TabStop = false;
+            this.pic7.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.BackColor = System.Drawing.Color.Black;
+            this.pic5.Location = new System.Drawing.Point(354, 204);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(97, 102);
+            this.pic5.TabIndex = 10;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // txtPlayer1
+            // 
+            this.txtPlayer1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.txtPlayer1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayer1.Location = new System.Drawing.Point(481, 69);
+            this.txtPlayer1.Multiline = true;
+            this.txtPlayer1.Name = "txtPlayer1";
+            this.txtPlayer1.Size = new System.Drawing.Size(110, 31);
+            this.txtPlayer1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(488, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Player 1 Name (X) :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(488, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Player 2 Name (O) :";
+            // 
+            // txtPlayer2
+            // 
+            this.txtPlayer2.BackColor = System.Drawing.Color.MediumOrchid;
+            this.txtPlayer2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayer2.Location = new System.Drawing.Point(481, 126);
+            this.txtPlayer2.Multiline = true;
+            this.txtPlayer2.Name = "txtPlayer2";
+            this.txtPlayer2.Size = new System.Drawing.Size(110, 32);
+            this.txtPlayer2.TabIndex = 13;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.btnNewGame.Location = new System.Drawing.Point(481, 180);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(110, 41);
+            this.btnNewGame.TabIndex = 15;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseCompatibleTextRendering = true;
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ADelongAssignment2.Properties.Resources.galaxy;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(603, 521);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPlayer2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlayer1);
+            this.Controls.Add(this.pic5);
+            this.Controls.Add(this.pic7);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic0);
+            this.Controls.Add(this.pic8);
+            this.Controls.Add(this.pic4);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.pic6);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.Name = "TicTacToeForm";
+            this.Text = "Tic Tac Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic0;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.TextBox txtPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlayer2;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ ADelongAssignment2/TicTacToeForm.cs
@@ -0,0 +1,159 @@
+﻿/*Purpose:              PROG 2370 Assignment 2             
+ *Created By:           Aubrey Delong 7677545          
+ * 
+ * Revision History:    Created Oct.5'2017
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADelongAssignment2
+{
+    /// <summary>
+    /// Main Class for TicTacToe
+    /// </summary>
+    public partial class TicTacToeForm : Form
+    {
+        const int NUMBER_OF_TURNS = 9;
+        int clickNumber = 0;
+        Bitmap x = ADelongAssignment2.Properties.Resources.x2;
+        Bitmap o = ADelongAssignment2.Properties.Resources.o2;
+
+        /// <summary>
+        /// the constructor for TicTacToe class
+        /// </summary>
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Set image of picturebox
+        /// Also, determine who wins, based on return value of CheckResult()
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            //holds the value of the picbox most recently clicked
+            PictureBox currentPic = (PictureBox)sender;
+
+            if (clickNumber % 2 == 0)
+            {
+                currentPic.Image = x;
+                clickNumber++;
+                currentPic.Enabled = false;
+            }
+            else
+            {
+                currentPic.Image = o;
+                clickNumber++;
+                currentPic.Enabled = false;
+            }
+
+            if (CheckResult() == true)
+            {
+                if(currentPic.Image == x)
+                {
+                    MessageBox.Show(txtPlayer1.Text+" wins!");
+                }
+                else
+                {
+                    MessageBox.Show(txtPlayer2.Text +" wins!");
+                }
+
+                StartNewGame();
+            }
+            else if (clickNumber == NUMBER_OF_TURNS)
+            {
+                MessageBox.Show("Tie Game!");
+                StartNewGame();
+            }
+
+        }
+
+        /// <summary>
+        /// Detemine if a player has won by comparing images
+        /// </summary>
+        /// <returns></returns>
+        bool CheckResult()
+        {
+            //check for horizontal win            
+            if (pic0.Image == pic1.Image && pic1.Image == pic2.Image && pic1.Image != null)
+            {
+                return true;
+            }
+            else if (pic3.Image == pic4.Image && pic4.Image == pic5.Image && pic3.Image != null)
+            {
+                return true;
+            }
+            else if (pic6.Image == pic7.Image && pic7.Image == pic8.Image && pic6.Image != null)
+            {
+                return true;
+            }
+
+            //check for a vertical win            
+            else if (pic0.Image == pic3.Image && pic3.Image == pic6.Image && pic0.Image != null)
+            {
+                return true;
+            }
+            else if (pic1.Image == pic4.Image && pic4.Image == pic7.Image && pic1.Image != null)
+            {
+                return true;
+            }
+            else if (pic2.Image == pic5.Image && pic5.Image == pic8.Image && pic2.Image != null)
+            {
+                return true;
+            }
+
+            //check for diagonal win
+            else if (pic0.Image == pic4.Image && pic4.Image == pic8.Image && pic0.Image != null)
+            {
+                return true;
+            }
+            else if (pic2.Image == pic4.Image && pic4.Image == pic6.Image && pic2.Image != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Set all images to null and enable all picture boxes
+        /// </summary>
+        public void StartNewGame()
+        {
+            //restart the game (make all picBox's empty)
+            pic0.Image = pic1.Image = pic2.Image = pic3.Image = pic4.Image = pic5.Image = pic6.Image = pic7.Image = pic8.Image = null;
+
+            //enable all buttons so they can be clicked again
+            pic0.Enabled = pic1.Enabled = pic2.Enabled = pic3.Enabled = pic4.Enabled = pic5.Enabled = pic6.Enabled = pic7.Enabled = pic8.Enabled = true;
+
+            //restart counter
+            clickNumber = 0;
+        }
+
+        /// <summary>
+        /// call StartNewGame method when user clicks button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
+
+
+
